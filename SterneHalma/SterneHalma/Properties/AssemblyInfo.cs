cc8fd74c254938fe0639e32238f422b2d5bcd3fe
Justin <tribekiller911@gmail.com>
2016-03-10 10:07:59 -0800

    monogame created

--- SterneHalma/SterneHalma/Properties/AssemblyInfo.cs
+++ SterneHalma/SterneHalma/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sterm_Halma")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SterneHalma")]
+[assembly: AssemblyProduct("SterneHalma")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sterm_Halma")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a5580483-1e44-4c8e-ba3c-ca5cd5f9288f")]
+[assembly: Guid("14b9375d-3a53-42e4-a281-ffecff4c8689")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.4.0.456")]
+[assembly: AssemblyFileVersion("3.4.0.456")]

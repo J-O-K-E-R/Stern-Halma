5b5891d0e378b6d09f48085a544314277c2e848c
Chun-Yip <koolman987654321@hotmail.com>
2016-03-28 20:49:19 -0700

    Client Server Takes a string written by server/client and prints it back out to console Async connection so that doesn't crash program if server exits.

--- SpriteandDraw/SpriteandDraw/Host.cs
+++ SpriteandDraw/SpriteandDraw/Host.cs
@@ -2,8 +2,124 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
 
-namespace SpriteandDraw {
-    class Host {
+
+namespace SterneHalma
+{
+    class CreateServer
+    {
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        /// <summary>
+        /// Empty Constructor for now. May fill it later if needed.
+        /// </summary>
+        public CreateServer()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates the Server and begins listeneing
+        /// </summary>
+        public void Create()
+        {
+            //buffer for incoming data
+            byte[] bytes = new Byte[1024];
+
+            //Gets the hosts DNS and endpoint for socket
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            //Have to write out the IP Address onto the host screen 
+
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                while (true)
+                {
+                    allDone.Reset();
+
+                    Console.WriteLine("Waiting for Connection");     //change to outpu on the screen
+                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+                    allDone.WaitOne();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("Press Enter to continue...");
+            Console.Read();
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            allDone.Set();
+
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize,
+                0, new AsyncCallback(ReadCallback), state);
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+
+                content = state.sb.ToString();
+                if (content.IndexOf("EOF") > -1)
+                {
+                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}", content.Length, content);
+                    Send(handler, content);
+                }
+                else
+                {
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+                }
+            }
+        }
+
+        private static void Send(Socket handler, String data)
+        {
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+            handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket handler = (Socket)ar.AsyncState;
+                int bytesSend = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSend);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
     }
 }
